8cde11a2ef9122eed8c54c4b7fbc2ff7ba8c177e
Akhil Gurjar <akhilgurjarji2508@gmail.com>
2025-07-20 17:50:59 +0530

    Player Move Script

--- Assets/Scripts/NetworkManaerUI.cs
+++ Assets/Scripts/NetworkManaerUI.cs
@@ -1,16 +1,26 @@
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkManaerUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        serverBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        hostBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
     }
 }
