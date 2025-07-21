f347a2d4e3f2284f243ce67dbf375b383f078cde
Akhil Gurjar <akhilgurjarji2508@gmail.com>
2025-07-21 10:15:24 +0530

    Add Network Spawner and Network transform

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,11 +1,35 @@
 using Unity.Netcode;
 using UnityEngine;
-
+using System.Collections.Generic;
 public class PlayerNetwork : NetworkBehaviour
 {
+    [SerializeField] private Transform spawnedObjectPrefab;
+    private Transform spawnedObjectTransform;
+
+    private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    
+
+    public override void OnNetworkSpawn()
+    {
+        randomNumber.OnValueChanged += (int previousValue, int newValue) =>
+        {
+            Debug.Log(randomNumber.Value);
+        };
+    }
     private void Update()
     {
         if(!IsOwner)  return;
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+            // randomNumber.Value = Random.Range(0, 100);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Destroy(spawnedObjectTransform.gameObject);
+        }
         Vector3 moveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
         if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
@@ -14,4 +38,5 @@ public class PlayerNetwork : NetworkBehaviour
         float moveSpeed = 3f;
         transform.position += moveDir * moveSpeed * Time.deltaTime;
     }
+    
 }
